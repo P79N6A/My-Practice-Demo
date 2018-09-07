a5935f229c923990e64c00c1fc7107b146ecd4b8
qingtianiii <1048601204@qq.com>
2018-09-07 16:10:31 +0800

    .

---
+++ js/type.js
@@ -0,0 +1,21 @@
+var num = 1
+var str = 'haha'
+var obj = {}
+var arr = []
+var func = function () {}
+
+// // NaN
+// console.log(typeof NaN)
+// console.log(NaN == NaN)
+
+// var a = 111
+// console.log(a.toString())
+// console.log(typeof a.toString())
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof null)
+
+console.log(typeof {})
+console.log(typeof [])
+console.log(typeof function() {})
