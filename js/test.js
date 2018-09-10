f2f3a9139df67b478f7f5eea0363af1ddcab2a3c
qingtianiii <1048601204@qq.com>
2018-09-10 19:32:29 +0800

    .

--- js/test.js
+++ js/test.js
@@ -7,20 +7,29 @@
 //     }
 //   }
 // }
-var oldTime = +new Date()
-for (var i = 0; i < 1000; i++) {
-  console.log(1)
-}
-var newTime = +new Date()
-console.log('time ', newTime - oldTime)
+// var oldTime = +new Date()
+// for (var i = 0; i < 1000; i++) {
+//   console.log(1)
+// }
+// var newTime = +new Date()
+// console.log('time ', newTime - oldTime)
 
-var oldTime = +new Date()
-for (var i = 0; i < 1000; i++) {
-  var a = 1
-  b = 1
-  if (a === b) {
-    console.log('xixi')
-  }
+// var oldTime = +new Date()
+// for (var i = 0; i < 1000; i++) {
+//   var a = 1
+//   b = 1
+//   if (a === b) {
+//     console.log('xixi')
+//   }
+// }
+// var newTime = +new Date()
+// console.log('time ', newTime - oldTime)
+
+function* gen () {
+  var a = yield [ Promise.resolve(1),
+    Promise.resolve(2)]
+  console.log(a)
 }
-var newTime = +new Date()
-console.log('time ', newTime - oldTime)
+
+var g = gen()
+console.log(g.next())
