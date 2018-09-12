2c9b672fa696bc98dc6339f301ac4a178e93368e
qingtianiii <1048601204@qq.com>
2018-09-12 16:07:15 +0800

    .

--- js/test.js
+++ js/test.js
@@ -25,11 +25,51 @@
 // var newTime = +new Date()
 // console.log('time ', newTime - oldTime)
 
-function* gen () {
-  var a = yield [ Promise.resolve(1),
-    Promise.resolve(2)]
-  console.log(a)
-}
+// var arr = [1,2,3,4]
+// var a = arr.reduce((prev, currv) => {
+//   console.log('pr', prev)
+//   console.log('c', currv)
+//   return prev + currv
+// })
 
-var g = gen()
-console.log(g.next())
+// console.log(a)
+
+// function de (delay) {
+//   console.log('delay', delay)
+  
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(`延迟${delay}执行啦`)
+//       resolve(delay)
+//     }, delay)
+//   })
+// }
+
+// async function test () {
+//   var timers = [3000, 4000, 2000, 1000]
+//   var t = timers.map(async delay => {
+//     var m = await de(delay)
+//     return m
+//   })
+  
+//   for (var item of t) {
+//     console.log('item', await item)
+//   }
+// }
+
+// test()
+
+
+(function () {
+  function foo () {
+    console.log('function')
+  }
+  var foo = undefined
+
+  console.log(foo) // undefined
+}())
+
+(function () {
+  console.log(aaa)
+  aaa = 100
+}())
