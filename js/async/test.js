92356e9cae2c6d2b45f02651b874c83536ec02b9
qingtianiii <1048601204@qq.com>
2019-01-16 18:43:17 +0800

    .

--- js/async/test.js
+++ js/async/test.js
@@ -1,40 +1,22 @@
-// var p = new Promise(resolve => resolve(1))
-
-// var p1 = p.then(v => {
-//   return obj
-// })
-
-// // thenable object
-// var obj = {
-//   then: function (resolve, reject) {
-//     resolve()
-//   }
-// }
-
-
-// var p = new Promise((resolve, rejected) => {
-//   rejected(1)
-// })
-
-// var p1 = p.then(null, (e) => {
-//   console.log(e)
-//   return new Promise((resolve) => resolve(33))
-// })
-
-// var p = new Promise(resolve => resolve(1))
-
-// var p1 = p.then((v) => {
-//   throw new Error('ha')
-//   return Promise.resolve(3)
-// }, (e) => {
-//   console.log('haaaaa')
-// })
-
-var p = new Promise((r) => {
-  throw new Error('xx')
-  r(1)
+setTimeout(() => {
+  console.log(0)
 })
 
-p.then(v => {
-  console.log(v)
+new Promise(resolve => {
+  resolve(1)
+  Promise.resolve().then(t => {
+    console.log(2)
+    new Promise(resolve => {
+      resolve(6)
+    })
+    .then(t => {
+      console.log(t)
+    })
+  })
+  console.log(3)
 })
+.then(t => {
+  console.log(t)
+})
+
+console.log(5)
